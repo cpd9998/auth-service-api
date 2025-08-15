0d2113489ad7e63304bcf9dcb5298f17525081f1
Isuru Wickramasinghe <udarawickramasinghe88@gmail.com>
2025-08-15 14:38:19 +0530

    add dependency

--- src/main/java/com/cpd/hotel_system/auth_service_api/AuthServiceApiApplication.java
+++ src/main/java/com/cpd/hotel_system/auth_service_api/AuthServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.cpd.hotel_system.auth_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthServiceApiApplication {
 
 	public static void main(String[] args) {
