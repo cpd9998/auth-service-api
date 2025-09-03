308deab75d26cd8c0975bd81c26045ba456cc9d9
udara88 <udarawickramasinghe88@gmail.com>
2025-09-03 09:13:17 +0530

    implemented create user controller

--- src/main/java/com/cpd/hotel_system/auth_service_api/api/UserController.java
+++ src/main/java/com/cpd/hotel_system/auth_service_api/api/UserController.java
@@ -1,9 +1,18 @@
 package com.cpd.hotel_system.auth_service_api.api;
 
+import com.cpd.hotel_system.auth_service_api.config.JwtService;
+import com.cpd.hotel_system.auth_service_api.dto.request.SystemUserRequestDto;
 import com.cpd.hotel_system.auth_service_api.service.SystemUserService;
+import com.cpd.hotel_system.auth_service_api.util.StandardResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/user-service/api/v1/users")
@@ -11,5 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final SystemUserService systemUserService;
     private final JwtService jwtService;
+
+    @PostMapping("/visitors/signup")
+    public ResponseEntity<StandardResponseDto> createUser(@RequestBody SystemUserRequestDto dto) throws IOException {
+
+        systemUserService.createUser(dto);
+        return new ResponseEntity<>(new StandardResponseDto(201,"user account was created",null), HttpStatus.CREATED);
+
+    }
 
 }
