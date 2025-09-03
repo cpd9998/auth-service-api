61a4cc91dfa0bad0ffbcc592fa8e6cc629a67e55
udara88 <udarawickramasinghe88@gmail.com>
2025-09-03 08:40:27 +0530

    implemented Security config

--- src/main/java/com/cpd/hotel_system/auth_service_api/config/JwtAuthConverter.java
+++ src/main/java/com/cpd/hotel_system/auth_service_api/config/JwtAuthConverter.java
@@ -7,12 +7,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken>{
 
     @Override
